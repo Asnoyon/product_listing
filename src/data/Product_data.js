397a12fb9afc006682ae837847f1f4d79c53a41b
Asnoyon <blackpain1100@gmail.com>
2023-07-17 23:50:20 +0600

    first commit

---
+++ src/data/Product_data.js
@@ -0,0 +1,75 @@
+const product_card = [
+    {
+        id:1,
+        product_name:"Amd Rayzen",
+        description: "Hands-free , Hads-on Music Experience",
+        price: 700,
+        thumb: "./images/1.png",
+        currency:"$"
+    },
+    {
+        id:2,
+        product_name:"Amd Rayzen",
+        description: "Hands-free , Hads-on Music Experience",
+        price: 700,
+        currency:"$",
+        thumb: "./images/1.png"
+    },
+    {
+        id:3,
+        product_name:"Amd Rayzen",
+        description: "Hands-free , Hads-on Music Experience",
+        price: 700,
+        currency:"$",
+        thumb: "./images/1.png"
+    },
+    {
+        id:4,
+        product_name:"Amd Rayzen",
+        description: "Hands-free , Hads-on Music Experience",
+        price: 700,
+        thumb: "./images/1.png",
+        currency:"$"
+    },
+    {
+        id:5,
+        product_name:"Amd Rayzen",
+        description: "Hands-free , Hads-on Music Experience",
+        price: 700,
+        currency:"$",
+        thumb: "./images/1.png"
+    },
+    {
+        id:6,
+        product_name:"Amd Rayzen",
+        description: "Hands-free , Hads-on Music Experience",
+        price: 700,
+        currency:"$",
+        thumb: "./images/1.png"
+    },
+    {
+        id:7,
+        product_name:"Amd Rayzen",
+        description: "Hands-free , Hads-on Music Experience",
+        price: 700,
+        thumb: "./images/1.png",
+        currency:"$"
+    },
+    {
+        id:8,
+        product_name:"Amd Rayzen",
+        description: "Hands-free , Hads-on Music Experience",
+        price: 700,
+        currency:"$",
+        thumb: "./images/1.png"
+    },
+    {
+        id:9,
+        product_name:"Amd Rayzen",
+        description: "Hands-free , Hads-on Music Experience",
+        price: 700,
+        currency:"$",
+        thumb: "./images/1.png"
+    }
+]
+export default product_card;
